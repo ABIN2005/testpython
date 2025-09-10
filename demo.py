aec83e6f2411c448f4a15921334c1906e4a5c2e3
ABIN2005 <abinashgiri09@gmail.com>
2025-09-10 23:57:59 +0530

    updated change

--- demo.py
+++ demo.py
@@ -1,2 +1,3 @@
 print("today we will learn python f string this is known as formatted string") 
 print(f"The temperature 75F in degrees celsius is {(75 - 32) * 5 / 9}C")
+print ("Isabel is 28 years old.")
