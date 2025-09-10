a5062433cf63c93542998b6b1db2ad34420ee75d
ABIN2005 <abinashgiri09@gmail.com>
2025-09-11 00:07:47 +0530

    uc

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
 print("today we will learn python f string this is known as formatted string") 
 print(f"The temperature 75F in degrees celsius is {(75 - 32) * 5 / 9}C")
+print("""There, I have written f (for formatted) before the quotation marks that denote a string, and I included the computation within curly braces {(75 - 32) * 5 / 9}. 
+The f character tells Python that the subsequent string has data or computations within any pair of curly braces.""")
 print ("Isabel is 28 years old.")
