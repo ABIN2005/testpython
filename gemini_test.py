b32f9deadbc0b66bd3c5e44329cc35bcb4a1d8bb
ABIN2005 <abinashgiri09@gmail.com>
2025-09-15 21:14:43 +0530

    changes6

--- gemini_test.py
+++ gemini_test.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 genai.configure(api_key="AIzaSyB2HoIfYQeIcZkZi33TkPd3JQ3o81abhmM")
 
 # Use correct model
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-2.5-flash")
 
 prompt = "Explain blockchain in simple words."
 response = model.generate_content(prompt)
